7cdabe466a902f86445f635982b186982f862c4c
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-08-19 11:50:48 +0530

    frist commit

---
+++ resources/js/components/AppSidebar.vue
@@ -0,0 +1,134 @@
+<!-- <script setup lang="ts">
+import NavFooter from '@/components/NavFooter.vue';
+import NavMain from '@/components/NavMain.vue';
+import NavUser from '@/components/NavUser.vue';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/vue3';
+import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import AppLogo from './AppLogo.vue';
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+        icon: LayoutGrid,
+    },
+];
+
+const footerNavItems: NavItem[] = [
+    {
+        title: 'Github Repo',
+        href: 'https://github.com/laravel/vue-starter-kit',
+        icon: Folder,
+    },
+    {
+        title: 'Documentation',
+        href: 'https://laravel.com/docs/starter-kits#vue',
+        icon: BookOpen,
+    },
+];
+</script>
+
+<template>
+    <Sidebar collapsible="icon" variant="inset">
+        <SidebarHeader>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton size="lg" as-child>
+                        <Link :href="route('dashboard')">
+                            <AppLogo />
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+
+        <SidebarContent>
+            <NavMain :items="mainNavItems" />
+        </SidebarContent>
+
+        <SidebarFooter>
+            <NavFooter :items="footerNavItems" />
+            <NavUser />
+        </SidebarFooter>
+    </Sidebar>
+    <slot />
+</template> -->
+<script setup lang="ts">
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-vue-next"
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Inbox",
+        url: "#",
+        icon: Inbox,
+    },
+    {
+        title: "Calendar",
+        url: "#",
+        icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "#",
+        icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+];
+</script>
+
+<template>
+    <Sidebar>
+        <SidebarContent>
+            <SidebarGroup>
+                <!-- <SidebarGroupLabel>Application</SidebarGroupLabel> -->
+
+
+
+
+                <!-- <template> -->
+                <Avatar class="w-14 h-14 mb-4">
+                    <AvatarImage src="https://github.com/unovue.png" alt="@unovue"
+                        class="w-14 h-14 object-cover rounded-full" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+
+                <!-- </template> -->
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem v-for="item in items" :key="item.title">
+                            <SidebarMenuButton asChild>
+                                <a :href="item.url">
+                                    <component :is="item.icon" />
+                                    <span>{{ item.title }}</span>
+                                </a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+    </Sidebar>
+</template>
---
+++ resources/js/components/AppSidebarHeader.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import Breadcrumbs from '@/components/Breadcrumbs.vue';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import type { BreadcrumbItemType } from '@/types';
+import ThemeToggle from './ThemeToggle.vue';
+
+withDefaults(
+    defineProps<{
+        breadcrumbs?: BreadcrumbItemType[];
+    }>(),
+    {
+        breadcrumbs: () => [],
+    },
+);
+</script>
+
+<template>
+  <header
+    class="flex h-16 shrink-0 items-center border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear md:px-4"
+  >
+    <div class="flex items-center w-full justify-between">
+      <!-- Left Section -->
+      <div class="flex items-center gap-3">
+        <SidebarTrigger class="-ml-1" />
+
+        <template v-if="breadcrumbs && breadcrumbs.length > 0">
+          <Breadcrumbs :breadcrumbs="breadcrumbs" />
+        </template>
+      </div>
+
+      <!-- Right Section -->
+      <div class="flex items-center gap-3">
+        <ThemeToggle />
+      </div>
+    </div>
+  </header>
+</template>
+
